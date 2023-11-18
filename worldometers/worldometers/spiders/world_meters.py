98429982a1a358e753c72600a94f8e5663dcaae7
zakashahmurzada <109436837+zakashahmurzada@users.noreply.github.com>
2023-11-18 23:43:47 +0400

    first commit

---
+++ worldometers/worldometers/spiders/world_meters.py
@@ -0,0 +1,19 @@
+import scrapy
+
+
+class WorldMetersSpider(scrapy.Spider):
+    name = "world_meters"
+    allowed_domains = ["www.worldometers.info/"]
+    start_urls = ["https://www.worldometers.info/world-population/population-by-country/"]
+
+    def parse(self, response):
+        countries = response.xpath('//td/a')
+
+        for country in countries:
+            name = country.xpath(".//text()").get()
+            link = country.xpath(".//@href").get()
+
+            yield {
+                'country_name' : name,
+                'country_link' : link
+            }
