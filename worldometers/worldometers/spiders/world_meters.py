8b2409c1c34c9fe1abd8193cfc2ba738fd9f181e
zakashahmurzada <109436837+zakashahmurzada@users.noreply.github.com>
2023-11-19 02:46:10 +0400

    added three categories

--- worldometers/worldometers/spiders/world_meters.py
+++ worldometers/worldometers/spiders/world_meters.py
@@ -1,9 +1,9 @@
 import scrapy
-
+import logging
 
 class WorldMetersSpider(scrapy.Spider):
     name = "world_meters"
-    allowed_domains = ["www.worldometers.info/"]
+    allowed_domains = ["www.worldometers.info"]
     start_urls = ["https://www.worldometers.info/world-population/population-by-country/"]
 
     def parse(self, response):
@@ -13,7 +13,19 @@ class WorldMetersSpider(scrapy.Spider):
             name = country.xpath(".//text()").get()
             link = country.xpath(".//@href").get()
 
+            # absolute_url = f"https://www.worldometers.info{link}"
+            # absolute_url = response.urljoin(link)
+
+            yield response.follow(url=link, callback = self.parse_country)
+
+    
+    def parse_country(self, response):
+        rows = response.xpath("(//table[@class='table table-striped table-bordered table-hover table-condensed table-list'])[1]/tbody/tr")
+        for row in rows:
+            year = row.xpath('.//td[1]/text()').get()
+            population = row.xpath('.//td[2]/strong/text()').get()
+
             yield {
-                'country_name' : name,
-                'country_link' : link
+                'year': year,
+                'population': population
             }
