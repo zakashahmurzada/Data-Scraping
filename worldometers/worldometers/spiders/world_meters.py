a38e3dd82eff11d502690a21cd8cf81f8996f065
zakashahmurzada <109436837+zakashahmurzada@users.noreply.github.com>
2023-11-20 00:17:03 +0400

    Added name of countries

--- worldometers/worldometers/spiders/world_meters.py
+++ worldometers/worldometers/spiders/world_meters.py
@@ -16,16 +16,18 @@ class WorldMetersSpider(scrapy.Spider):
             # absolute_url = f"https://www.worldometers.info{link}"
             # absolute_url = response.urljoin(link)
 
-            yield response.follow(url=link, callback = self.parse_country) # At here in self.parse_country we are calling the below function
+            yield response.follow(url=link, callback = self.parse_country, meta={'country_name': name}) # At here in self.parse_country we are calling the below function
 
     
     def parse_country(self, response):
+        name = response.request.meta['country_name']
         rows = response.xpath("(//table[@class='table table-striped table-bordered table-hover table-condensed table-list'])[1]/tbody/tr")
         for row in rows:
             year = row.xpath('.//td[1]/text()').get() # Brings year part from each row which index of td is 1 stands for year
             population = row.xpath('.//td[2]/strong/text()').get() # td index 2 contains population inside it
 
             yield {  # with the help of yield function we are returning them back
+                'country_name' : name,
                 'year': year,  
                 'population': population
             }
